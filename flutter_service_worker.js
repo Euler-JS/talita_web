d56281f0af01fe40fd5bed011add2ed553a9185d
Euler-JS <joaoalbertojose96@gmail.com>
2025-06-25 12:43:58 +0200

    vv

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f8505d4f62ffbc0177ab2d85f9b39fa5",
+const RESOURCES = {"flutter_bootstrap.js": "c3ce78e1b50ec0ea9da0ba78016fa173",
 "version.json": "132c2b8af07cf9b31355ee29ad9d9d01",
-"index.html": "39db34dabf5b03e44d3611ea1037c2cd",
-"/": "39db34dabf5b03e44d3611ea1037c2cd",
+"index.html": "8086d63b16a60ebd4ddc81e07c97f057",
+"/": "8086d63b16a60ebd4ddc81e07c97f057",
 "main.dart.js": "305f9896ec4cb02a991dff6d692d7b77",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
